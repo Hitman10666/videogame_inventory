0c01ec918f5273e0ab6ac8d3fc4c4039fc47b342
Viktor <stay.in.shadow@gmail.com>
2018-04-13 15:21:52 -0400

    hello my 2nd commit

--- VideoGameInventory/src/prog24178/javafx/GameList.java
+++ VideoGameInventory/src/prog24178/javafx/GameList.java
@@ -7,24 +7,158 @@ Other Files in this Project:
   GameList.java
   Platform.java
  */
-
 package prog24178.javafx;
 
 import java.util.ArrayList;
 
 /**
- * This class represents a list of video games, as an ArrayList. It is a 
+ * This class represents a list of video games, as an ArrayList. It is a
  * collection of Game objects. It will allow for the sorting, display, editing,
- * adding, and removing records of video games. 
- * 
+ * adding, and removing records of video games.
+ *
  * @see Game
  * @see Platform
- * 
+ *
  * @author Viktor Langeryt
  * @author Robert Verdi
  */
 public class GameList {
 
-    private ArrayList<Game> gameList = new ArrayList();
-    
+	private ArrayList<Game> gameList = new ArrayList();
+
+	/**
+	 * Default GameList constructor
+	 *
+	 */
+	public GameList() {
+	}
+
+	/**
+	 * Constructor that accepts a variable list of Game objects.
+	 *
+	 * @param g Game object(s)
+	 */
+	public GameList(Game... g) {
+	}
+	
+	/**
+	 * Constructor that accepts a value for the index of a Game object in 
+	 * the list of Games.
+	 * 
+	 * @param index the index of a Game in the list of games.
+	 */
+	public GameList(int index) {		
+	}
+	
+	/**
+	 * Constructor that accepts both an index and a Game object value.
+	 * 
+	 * @param index an index in the list of Games
+	 * @param g a Game object
+	 */
+	public GameList (int index, Game g) {
+	}
+
+	/**
+	 * Retrieves a particular Game object from the ArrayList of Games.
+	 *
+	 * @param index the index in the ArrayList of the desired object.
+	 * @return the Game object at the specified index.
+	 * @throws IndexOutOfBoundsException if the given index is not in bounds of
+	 * the current array list.
+	 */
+	public Game get(int index) {
+		if (index >= 0 && index < gameList.size()) {
+			return gameList.get(index);
+		} else {
+			throw new IndexOutOfBoundsException("Error: Index must be greater "
+					+ "than zero");
+		}
+	}
+
+	/**
+	 * Replaces an already existing Game object in the list with another.
+	 *
+	 * @param index the index of the Game to be replaced
+	 * @param g the Game object to be placed at the specified index.
+	 * @throws IndexOutOfBoundsException if the index does not already exist in
+	 * the array list.
+	 */
+	public void set(int index, Game g) {
+		if (index > 0 && index < gameList.size()) {
+			gameList.set(index, g);
+		} else {
+			throw new IndexOutOfBoundsException("Error: The index does not "
+					+ "exist in the array list yet");
+		}
+	}
+
+	/**
+	 * Adds a Game object to the ArrayList.
+	 *
+	 * @param g the Game object to add to the list.
+	 */
+	public void add(Game g) {
+		// rough method
+		gameList.add(g);
+	}
+
+	/**
+	 * Removes from the list a Game object at a specified index.
+	 *
+	 * @param index index in the list of the Game object to be removed.
+	 * @throws IndexOutOfBoundsException when the given index is not in bounds
+	 * of the current the array list
+	 */
+	public void remove(int index) {
+		if (index >= 0 && index < gameList.size()) {
+			gameList.remove(index);
+		} else {
+			throw new IndexOutOfBoundsException("Error: Index not found in "
+					+ "bounds of the current array list.");
+		}
+	}
+
+	/**
+	 * Returns the index value of a specific Game object.
+	 *
+	 * @param g a Game object to be located.
+	 * @return the index of the particular Game. Will return -1 if the Game is
+	 * not in the list.
+	 */
+	public int indexOf(Game g) {
+		return gameList.indexOf(g);
+	}
+
+	/**
+	 * Checks to see if a Game object is in the gameList.
+	 *
+	 * @param g a Game object to search for
+	 * @return true or false, depending on whether the Game is in the list.
+	 */
+	public boolean contains(Game g) {
+		return gameList.contains(g);
+	}
+
+	/**
+	 * Retrieves the size of the list of games.
+	 *
+	 * @return the size of the list of games.
+	 */
+	public int size() {
+		return gameList.size();
+	}
+	
+	public GameList findGameByName(String searchTitle) {
+		GameList searchResults = new GameList();
+		
+		return searchResults;
+	}
+
+	public GameList findGameByYear(int searchYear) {
+		GameList searchResults = new GameList();
+		
+		return searchResults;
+	}
+
 }
